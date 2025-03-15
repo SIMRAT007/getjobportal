68a9247b59896f963aed6d6771c2ab9db80053d1
Cart-Shop <54469728+SIMRAT007@users.noreply.github.com>
2025-03-15 12:24:19 +0530

    Header Comapny Logo Name and CSS

--- src/layouts/app-layout.jsx
+++ src/layouts/app-layout.jsx
@@ -10,7 +10,7 @@ const AppLayout = () => {
         <Outlet />
       </main>
       <div className="p-10 text-center bg-gray-800 mt-10">
-        Copyright © 2025 @Let's Hire
+        Copyright © 2025 @DestinyJobs
       </div>
     </div>
   );
--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -7,13 +7,13 @@ const LandingPage = () => {
     <main className="flex flex-col gap-10 sm:gap-20 py-10 sm:py-10">
       <section className="text-center pt-10">
       <p className="border-2 mb-5 py-2 px-2 rounded-3xl inline-block">Find Jobs and Recruiters <span className="bg-white text-black px-2 py-1 rounded-3xl">of your choice</span></p>       
-       <h1 className="flex flex-col items-center justify-center gradient-title font-extrabold text-4xl sm:text-6xl lg:text-8xl tracking-tighter py-4">
+       <h1 className="flex flex-col items-center justify-center gradient-title font-extrabold text-6xl sm:text-6xl lg:text-8xl tracking-tighter py-4 ">
         Supporting Job Seekers
           <span className="flex items-center gap-2 sm:gap-6">
             Every Step of the Way
           </span>
         </h1>
-        <p className="text-gray-400 sm:mt-4 text-xs sm:text-xl pt-10">
+        <p className="text-gray-400 sm:mt-4 text-xs sm:text-xl pt-10 max-md:pt-5">
         Unlock your true potential and discover a world of opportunities that align with your skills, interests, and aspirations.
         </p>
       </section>
